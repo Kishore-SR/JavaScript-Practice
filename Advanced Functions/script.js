2ce3231e7e5b2aa68300c26e382674e7eac6cfde
Kishore S R <kishoresr01@gmail.com>
2024-09-25 23:41:16 -0400

    add: JS functions

---
+++ Advanced Functions/script.js
@@ -0,0 +1,11 @@
+// Anonymus Functions as Values 
+let sum = function(num1, num2){
+    return num1 + num2;
+}
+console.log(sum(6,3));
+console.log(sum(sum(5,4),2)) //Same two number sum function can be used to calculate sum of 3 or more nos.
+
+
+
+
+
