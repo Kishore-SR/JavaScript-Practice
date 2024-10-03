eba9fe7c5a3a9a8824922154a292909f81168a0e
Kishore S R <kishoresr01@gmail.com>
2024-10-03 11:13:51 -0400

    add: js practice

--- JS Basics/script.js
+++ JS Basics/script.js
@@ -20,3 +20,26 @@ let age3= null;
 let finalAge = age3 ?? 18;
 console.log('Age: '+age3);
 console.log('Final Age: '+finalAge);
+
+
+// ||= Operator : If left side is falsy value -> Right side value is considered
+let a = ''; //Falsy values are: 0, Null, NaN, undefined, '', false, 0n
+a ||= 'Default Name';
+console.log(a);
+
+// &&= Operator: If left side is truthy value -> Right side value is considered 
+let b='Kishore S R'; //Truthy Value
+b &&= 'Default Name';
+console.log(b);
+
+// Using 'Underscores' (_) to improve readability of numbers 
+let num = 1_000_000;
+console.log(num + 5);
+let numFloat = 1_000_0.5;
+console.log(numFloat+8.5);
+
+// array.at() method : To access array (or) string elements both from 'start' and 'end' indexing (negative indexing)
+let array = [1,2,3,4,5,6];
+console.log(array.at(-1)); //OP: 6 (last element from the array)
+
+console.log(array.at(-3)); //OP: 4 (last 3rd element from the array)
